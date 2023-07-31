be9f5192a5519577acb9189d7e593b9cc408c6e2
Becky_Codes <rebeccabrowncodes@gmail.com>
2023-07-31 21:20:14 +0200

    adjusted fonts and added text to first book

--- src/components/common/GalleryItem.js
+++ src/components/common/GalleryItem.js
@@ -3,15 +3,16 @@ import galleryItem from "../../assets/gallery-items/gallery-item-1.jpg";
 import "../../styles/GalleryItems.css";
 
 function GalleryItem() {
-  const galleryItemText = "Conquest";
+  const firstGalleryItemText = "Conquest";
   const firstItemLink =
     "https://www.amazon.com/Conquest-Dragon-Bones-Book-1-ebook/dp/B083JY95R5";
 
   return (
     <div className="gallery-item">
       <a href={firstItemLink}>
-        <img src={galleryItem} alt={galleryItemText} />
+        <img src={galleryItem} alt={firstGalleryItemText} />
       </a>
+      <p className="gallery-item-text">{firstGalleryItemText}</p>
     </div>
   );
 }
